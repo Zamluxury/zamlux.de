b5e4f73b575c560b0af03c1928d658fb026c620a
Zamluxury <sadignagjev@gmail.com>
2026-06-29 22:27:35 +0200

    Revert "fix back button to home in ProductPage"
    This reverts commit fc495bf16a4aa83f2fb8e9fc6be3f8a8de854247.

--- fix_back2.py
+++
@@ -1,6 +0,0 @@
-﻿content = open('src/components/ProductPage.tsx', 'r', encoding='utf-8').read()
-old = "  const { addToCart } = useApp();"
-new = "  const { addToCart } = useApp();\n  React.useEffect(() => {\n    window.history.pushState(null, '', window.location.href);\n    const handleBack = () => { window.location.href = '/'; };\n    window.addEventListener('popstate', handleBack);\n    return () => window.removeEventListener('popstate', handleBack);\n  }, []);"
-content = content.replace(old, new)
-open('src/components/ProductPage.tsx', 'w', encoding='utf-8').write(content)
-print('Done')
