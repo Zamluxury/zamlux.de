eab0602d25445512764dc3f4c19daf4b6ffc8aba
Zamluxury <sadignagjev@gmail.com>
2026-06-29 20:13:09 +0200

    add currentProductSlug useState

--- src/App.tsx
+++ src/App.tsx
@@ -2686,6 +2686,7 @@ const CheckoutModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => vo
 export default function App() {
 const [currentPage, setCurrentPage] = useState<PageType>('home');
 const [isAuthOpen, setIsAuthOpen] = useState(false);
+  const [currentProductSlug, setCurrentProductSlug] = useState<string | null>(null);
 const [isCartOpen, setIsCartOpen] = useState(false);
 const [isCheckoutOpen, setIsCheckoutOpen] = useState(false);
 const pageToPath: Record<string, string> = {
